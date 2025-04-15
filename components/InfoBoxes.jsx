40ff56916a38f0e7c6f3c461ded33ab58da894f0
Tsigie Beyene <tsigiebey29@gmail.com>
2025-04-15 13:30:39 -0700

    adding info box feature

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -6,8 +6,23 @@ const InfoBoxes = () => {
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
             
-            <InfoBox/>  
-            <InfoBox/>
+            <InfoBox heading='For Renters'
+            
+              buttonInfo={{
+                text: 'Browse Properties',
+                link: '/properties',
+                backgroudColor: 'bg-black',
+              }}
+            > 
+            Find your dream rental property. Bookmark properties and contact owners. </InfoBox>  
+            <InfoBox heading='For Property Owners'
+            buttonInfo={{
+              text: 'Add Properties',
+              link: '/properties/add',
+              backgroudColor: 'bg-blue-500',
+            }}
+                 backgroudColor="bg-blue-100"
+            >  List your properties and reach potential tenants. Rent as an airbnb or long term. </InfoBox>
            
           </div>
         </div>
