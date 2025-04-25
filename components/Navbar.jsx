357d8de0144b09b19603c3cc1ebfae734a5076b7
Tsigie Beyene <tsigiebey29@gmail.com>
2025-04-25 14:15:36 -0700

    adding signIn and sign Up functionality

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
  'use client';
- import { useState } from 'react';
+ import { useState,useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -7,12 +7,25 @@ import logo from '@/assets/images/logo-white.png';
 import profileDefault from '@/assets/images/profile.png';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import {FaGoogle} from 'react-icons/fa';
+import { set } from 'mongoose';
 const Navbar = () => {
+    const {data: session } = useSession();
+    const profileImage= session?.user?.image ;
+
+
   const[ isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Example state for logged in status
-
+  const [providers, setProviders] = useState(null);
   const pathname = usePathname();
+
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    }
+    setAuthProviders(); 
+
+  },[]);
 
   return ( <nav className="bg-blue-700 border-b border-blue-500">
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -86,21 +99,26 @@ const Navbar = () => {
             </div>
   
             {/* <!-- Right Side Menu (Logged Out) --> */}
-            {!isLoggedIn && (
+            {!session && (
             <div className="hidden md:block md:ml-6">
               <div className="flex items-center">
-                <button
+                {providers && Object.values(providers).map((provider,index) => (
+                  <button key={index}
+                  onClick={() => signIn(provider.id)}
                   className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                 >
                   <FaGoogle className= "text-white mr-2" />
                   <span>Login or Register</span>
                 </button>
+                 ))}
+
+                
               </div>
             </div>
             )}
   
             {/* <!-- Right Side Menu (Logged In) --> */}
-            {isLoggedIn && (
+            {session && (
             <div
               className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
             >
@@ -148,7 +166,9 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage||profileDefault}
+                      width={40}
+                      height={40}
                       alt=""
                     />
                   </button>
@@ -186,6 +206,10 @@ const Navbar = () => {
                     role="menuitem"
                     tabIndex="-1"
                     id="user-menu-item-2"
+                    onClick={()=>{
+                      setIsProfileMenuOpen(false);
+                      signOut();
+                    }}
                   >
                     Sign Out
                   </button>
@@ -211,14 +235,14 @@ const Navbar = () => {
               className={`${pathname === '/properties' ? ' bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
               >Properties
             </Link>
-            {isLoggedIn && (  
+            {session && (  
             <Link
               href="/properties/add"
               className={`${pathname === '/properties/add' ? ' bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
               >Add Property
             </Link>
             )}
-            {!isLoggedIn && (
+            {!session && (
             <button
               className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-5"
             >
