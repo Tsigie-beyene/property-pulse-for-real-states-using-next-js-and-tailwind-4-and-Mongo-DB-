669e33a3f6332d4ba4d50d8aa1a8d9fd950d361a
Tsigie Beyene <tsigiebey29@gmail.com>
2025-05-03 13:38:10 -0700

    finish the first path

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 // import connectDB from "@/config/database";
 
 const HomePage = () => {
@@ -9,6 +10,7 @@ return <>
     
     <Hero />
     <InfoBoxes />
+    <FeaturedProperties />
     <HomeProperties />
     
     </>
